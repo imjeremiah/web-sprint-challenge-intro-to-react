1bb68c322eff3ca06a517ceb7fe40d045bf35bfa
Jeremiah Candelaria <jeremiahcandelaria@gmail.com>
2020-12-11 11:00:54 -0800

    complete mvp tasks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [character, setCharacter] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    const fetchCharacter = () => {
+      axios
+        .get("https://akabab.github.io/starwars-api/api/all.json")
+        .then((res) => {
+          console.log(res);
+          setCharacter(res.data)
+        })
+        .catch((err) => {
+          console.log("err", err)
+        })
+    }
+    fetchCharacter()
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {character.map(character => {
+        return(
+          <Character key={character.id} character={character}/>
+        )
+      })
+
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,37 @@
 // Write your Character component here
+import React from 'react';
+import { Container, Row, Col, CardDeck, Card, CardImg, CardText, CardBody, CardLink, CardTitle } from 'reactstrap';
+import styled from "styled-components";
+
+export default function Character(props) {
+    const { character } = props;
+    
+    return (
+        <StyledCharacter>
+            <Container>
+                <CardDeck>
+                    <Row>
+                        <Card>
+                            <CardImg src={character.image} alt={character.name}/>
+                            <CardBody>
+                                <CardTitle tag="h5">{character.name}</CardTitle>
+                                <CardLink href={character.wiki}>{character.name}'s Wiki Page</CardLink>
+                                <CardText>{character.name} is a {character.species} {character.gender} from {character.homeworld}.</CardText>
+                            </CardBody>
+                        </Card>
+                    </Row> 
+                </CardDeck>
+            </Container>
+        </StyledCharacter>
+    )
+
+}
+
+const StyledCharacter = styled.div`
+    .card {
+        width: 30%;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+`;
